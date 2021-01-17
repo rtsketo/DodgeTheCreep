c2d134a3e529511cfa7b29ca79e1740f3ccba058
Aristos Pasalides <rtsketo@gmail.com>
2021-01-17 14:49:15 +0200

    Set variable size on Mobs.

--- Mob.gd
+++ Mob.gd
@@ -3,11 +3,15 @@ extends RigidBody2D
 export var min_speed = 150
 export var max_speed = 250
 
+onready var size = .7 * randf() + .3
+
 
 func _ready():
+	$CollisionShape2D.set_scale(Vector2(size, size))
+	$AnimatedSprite.set_scale(Vector2(size*.75, size*.75))
 	var mob_types = $AnimatedSprite.frames.get_animation_names()
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
-
+	
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
